73b52c74abc16438b08c63612da58c7222db94fa
Ashikms1998 <142452260+Ashikms1998@users.noreply.github.com>
2024-04-06 14:56:16 +0530

    Chapter 13.03-introducing-JSX

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// React Element
 
-const parent = React.createElement("div",{id:'parent'},[
-React.createElement("div",{id:"child"},[
-React.createElement('h1',{},"This is the changed stuff vro"), 
-React.createElement('h2',{},"This is the second Child"),
-]),
-React.createElement("div",{id:"child2"},[
-React.createElement("h3",{},"This is child number 3"),
-React.createElement("h4",{},"This is child number 4"),
-]),
-]);
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const heading = React.createElement(
+    'h1',
+    { id: "heading" },
+    "Hello From the other side"
+);
+console.log(heading);
 
-root.render(parent)
+//JSX
+//(this code is transpiled before it reaches the JS)-PARCEL's babel module convert this
+//Inside this babel module their is 'createElement' like above method which converts this to the understandable format
+
+const jsxHeading = <h1 className="head" tabIndex="1">This is JSX heading</h1>
+console.log(jsxHeading);
+
+
+//The code on the React Element and JSX is same
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+root.render(jsxHeading);
