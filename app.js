bc29e19804ac13cf4edd31f3ff289350c2b597a7
Ashikms1998 <142452260+Ashikms1998@users.noreply.github.com>
2024-04-06 11:27:53 +0530

    Chapter-03.01-JustBeforeEnteringMain

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 
 const parent = React.createElement("div",{id:'parent'},[
 React.createElement("div",{id:"child"},[
-React.createElement('h1',{},"This is the first child"),
+React.createElement('h1',{},"This is the changed stuff vro"), 
 React.createElement('h2',{},"This is the second Child"),
 ]),
 React.createElement("div",{id:"child2"},[
