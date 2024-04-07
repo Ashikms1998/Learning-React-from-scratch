05cb4cc69a59ccae78e1ae60e84c074d231bce71
Ashikms1998 <142452260+Ashikms1998@users.noreply.github.com>
2024-04-07 14:41:42 +0530

    Chapter-03-completed

--- app.js
+++ app.js
@@ -1,25 +1,55 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
+//React Element
 
-const heading = React.createElement(
-    'h1',
-    { id: "heading" },
-    "Hello From the other side"
+const heading = (
+<h1 className="head" tabIndex="1">
+    This is JSX heading
+</h1>
 );
+
+const elem = <span>This is a new element</span>
+
 console.log(heading);
 
-//JSX
-//(this code is transpiled before it reaches the JS)-PARCEL's babel module convert this
-//Inside this babel module their is 'createElement' like above method which converts this to the understandable format
+const fn = () => true;
 
-const jsxHeading = <h1 className="head" tabIndex="1">This is JSX heading</h1>
-console.log(jsxHeading);
+const fn2= () => {
+    return true;
+};
+
+//above function fn and fn2 are same this format is what we have written down
+
+// React Component
+//1.Class based component -OLD
+//2.Functional compnent -NEW
+
+//REACT functional component
+
+const HeadingComponent = () =>{
+    return <h1 className="heading">This is functional component in JSX</h1>
+}
 
 
-//The code on the React Element and JSX is same
+const HeadingComponent2 = () =>( 
+    <div id="container">
+         {elem}
+        <h1>{"this is jS inside functional component"}</h1>
+        <h2>{2000+3000}</h2>
+        {heading} {/*Tthis is react element calling that we written above inside a functional componet */}
+         < HeadingComponent /> {/*calling another functional component written above so Thiscode is also called component composition*/}
+        <HeadingComponent></HeadingComponent> {/* The line above and this line is same */}
+        {HeadingComponent()}{/* You can also call this as a function */}
+        <h1 className="heading">This is functional component in JSX2</h1>
+    </div>
+);
+
+//above function HeadingComponent and HeadingComponent2 are same this format is what we have written down
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+//Rendering React Element
 root.render(heading);
-root.render(jsxHeading);
+
+//Rendering React Functional Component
+root.render(< HeadingComponent2 />)
